0def2803d4f37e7c9e399ebee3becae8c2f56f19
Omar <titoomar2008@gmai.com>
2021-09-07 19:54:24 -0300

    resuelto

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -34,6 +34,7 @@ function agregarMetodoPrototype(Constructor) {
 function agregarStringInvertida() {
   // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
   // El método debe llamarse "reverse"
+  //
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
